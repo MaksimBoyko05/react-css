3357c4c2467996f8ee098b5be7af230bc28f42fa
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-10-16 13:03:25 +0300

    added paralax

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import {
+  useViewportScroll,
+  motion,
+  useTransform,
+  useMotionValue,
+} from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const Animatedcircle = () => {
+import "./App.css";
+
+function App() {
+  const { scrollY } = useViewportScroll();
+  const y1 = useTransform(scrollY, [0, 300], [0, 200]);
+  const y2 = useTransform(scrollY, [0, 300], [0, -100]);
+
+  const [ref, inView, entry] = useInView({
+    threshold: 0.5,
+    triggerOnce: false,
+  });
+
+  console.log(entry);
+
+  const variants = {
+    visible: { opacity: 1, scale: 1, y: 0 },
+    hidden: {
+      opacity: 0,
+      scale: 0.65,
+      y: 50,
+    },
+  };
+
   return (
-    <motion.div
-      style={{
-        width: 50,
-        height: 50,
-        borderRadius: "50%",
-        backgroundColor: "blue",
-        position: "absolute",
-      }}
-      animate={{
-        x: [0, 100, 200, 150, 0], 
-        y: [0, 50, 100, 150, 200],
-        backgroundColor: ["#0000ff", "#00ff00", "#ff0000", "#ffff00", "#0000ff"], 
-        opacity: [1, 0.5, 0.8, 0.3, 1], 
-        scale: [1, 1.5, 0.8, 1.2, 1], 
-      }}
-      transition={{
-        duration: 5,
-        ease: "easeInOut",
-        loop: Infinity,
-      }}
-    />
+    <>
+      <motion.div className="box" style={{ y: y1, x: -50 }} />
+      <motion.div
+        className="box"
+        style={{ y: y2, x: 50, background: "salmon" }}
+      />
+      <div style={{ height: 500 }} />
+      <div style={{ position: "fixed", top: 0, left: 0 }}>
+        {" "}
+        {"is in view? " + inView}
+      </div>
+      <motion.div
+        animate={inView ? "visible" : "hidden"}
+        variants={variants}
+        transition={{ duration: 2, ease: "easeOut" }}
+        ref={ref}
+        className="magic"
+      />
+    </>
   );
-};
+}
 
-export default Animatedcircle;
+export default App;
