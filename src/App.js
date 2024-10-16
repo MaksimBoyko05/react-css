7af43e043ebb67c0e0f130bff400772003edb358
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-10-16 12:50:52 +0300

    added Animatedcircle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import React from "react";
+import { motion } from "framer-motion";
 
-import './App.css';
-
-function App() {
+const Animatedcircle = () => {
   return (
-    <div className="App">
-     <button class="btn">Press</button>
-     <div class="square">
-     </div>
-     <div class="object"></div>
-     <div class="circle">wooow</div>
-     <div class="square_2"></div>
-    </div>
+    <motion.div
+      style={{
+        width: 50,
+        height: 50,
+        borderRadius: "50%",
+        backgroundColor: "blue",
+        position: "absolute",
+      }}
+      animate={{
+        x: [0, 100, 200, 150, 0], 
+        y: [0, 50, 100, 150, 200],
+        backgroundColor: ["#0000ff", "#00ff00", "#ff0000", "#ffff00", "#0000ff"], 
+        opacity: [1, 0.5, 0.8, 0.3, 1], 
+        scale: [1, 1.5, 0.8, 1.2, 1], 
+      }}
+      transition={{
+        duration: 5,
+        ease: "easeInOut",
+        loop: Infinity,
+      }}
+    />
   );
-}
+};
 
-export default App;
+export default Animatedcircle;
